92ebb874e4b5f62403aecacdc04ca994332a4efe
Aditya Dumbre <66166460+adityad15@users.noreply.github.com>
2025-07-05 20:20:12 +0530

    Add files via upload

---
+++ app/src/main/java/eu/tutorials/myrecipeapp/Category.kt
@@ -0,0 +1,7 @@
+package eu.tutorials.myrecipeapp
+
+data class Category(val idCategory:String , val strCategory:String ,
+    val strCategoryThumb:String , val strCategoryDescription:String
+  )
+
+data class CategoriesResonse(val categories:List<Category>)
